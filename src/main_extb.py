f129de23060192a263bbbaf7008dc2e6443b35b6
Aarav Desai <144348378+CriticalCat9813@users.noreply.github.com>
2026-04-07 20:35:41 +0530

    .

--- src/main_extb.py
+++ src/main_extb.py
@@ -4,11 +4,6 @@ import src.solver_compare as solver_compare
 
 
 def main(n, objective, timeLimit, epsLimit, **kwargs):
-    """
-    Wrapper for both:
-      - baseline paper method (coalition_size_cap=None)
-      - Extension B / k-core (coalition_size_cap=k)
-    """
     coalition_size_cap = kwargs.get("coalition_size_cap", None)
     min_block_gain_mult = kwargs.get("min_block_gain_mult", 1.0)
     iterLimit = kwargs.get('iterLimit', 100)
@@ -35,7 +30,6 @@ def main(n, objective, timeLimit, epsLimit, **kwargs):
 
 
 if __name__ == '__main__':
-    # Example run: baseline paper method on Bengaluru.
     n = 1430
     objective = 'utilitarian'
     timeLimit = 300
